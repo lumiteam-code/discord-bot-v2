8d1464ef3dd89c7e062719f447013401a8f2039d
lumiteam-code <code001.lumiteam@outlook.com>
2026-04-13 06:17:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
 
 console.log("🚀 START FILE");
 
+const app = express();
+
+// 🔥 MỞ PORT để Render không kill
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("🌐 Server chạy port", PORT);
+});
+
+// ===== DISCORD BOT =====
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
